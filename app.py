471e340827894e16e732bd731fd58de40e2dd02a
Hunter <mosayaad93@gmail.com>
2026-06-13 19:03:31 +0300

    version 3

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-      return "Hello From Azure Container Apps - Version 2"
+    return "Version 3 - Auto Deployment Test"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=80)
